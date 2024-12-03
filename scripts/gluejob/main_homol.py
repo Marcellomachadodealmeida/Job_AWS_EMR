510768d7121ce101555234dd8f152eed6d632be7
Marcello Almeida <marcelloalmeidati@hotmail.com>
2024-12-03 13:30:20 -0300

    Update main_homol.py

--- scripts/gluejob/main_homol.py
+++ scripts/gluejob/main_homol.py
@@ -1,60 +1,42 @@
-
-import boto3
 import pandas as pd
 import re
-import sys
-from awsglue.utils import getResolvedOptions
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from pyspark.sql.functions import col
+from awsglue.utils import *
 
-# Função para ler o arquivo CSV do S3
-def read_csv_from_s3(bucket, key):
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket=bucket, Key=key)
-    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+# Inicializar SparkSession 
+spark = SparkSession.builder.appName("PandasToSparkDataFrame").getOrCreate()
 
-    if status == 200:
-        print(f"Successfully fetched object {key} from bucket {bucket}")
-        df = pd.read_csv(response.get("Body"))
-        return df
-    else:
-        print(f"Failed to fetch object {key} from bucket {bucket}. Status - {status}")
-        return None
+args = getResolvedOptions(sys.argv, ['s3_bucket','s3_input_path','s3_output_path'])
 
-# Função para salvar o DataFrame em Parquet no S3
-def save_parquet_to_s3(df, bucket, key):
-    buffer = io.BytesIO()
-    df.to_parquet(buffer, index=False)
-    s3 = boto3.client('s3')
-    s3.put_object(Bucket=bucket, Key=key, Body=buffer.getvalue())
+# Definir o caminho do arquivo CSV no S3
+s3_bucket_name = args['s3_bucket']
+s3_input_path_name = args['s3_input_path']
+s3_output_path_name = args['s3_output_path']
 
-# Configurar o contexto do Glue
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
+dtype = {6: 'str'}
+df = pd.read_csv('s3://bucket-glue-incremental/Homologacao/input_homol/a_processar/dados/',dtype=dtype)
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-job.init(args['JOB_NAME'], args)
+def remove_special_characters(text):
+    # Remove caracteres que não sejam letras ou números 
+    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
 
-# Parâmetros do bucket e do caminho
-input_bucket = 'bucket-glue-incremental'
-input_key = 's3://bucket-glue-incremental/Homologacao/input_homol/a_processar/dados/'
-output_bucket = 'bucket-glue-incremental'
-output_key = 's3://bucket-glue-incremental/Homologacao/output_homol/'
+df = df.apply(lambda x: x.astype(str).apply(remove_special_characters))
 
-# Ler o CSV do S3
-df = read_csv_from_s3(input_bucket, input_key)
+df = df.replace(r' ', '_', regex=True)
 
-if df is not None:
-    # Fazer o tratamento dos dados com pandas
-    # Exemplo: Remover caracteres especiais das colunas
-    df = df.applymap(lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', str(x)))
-    df = df.replace(r' ', '_', regex=True)
-    df=df.replace('nan','vazio')
-    # Salvar o DataFrame tratado em formato Parquet no S3
-    save_parquet_to_s3(df, output_bucket, output_key)
+colunas_solicitadas =['Name','Description','Violation Type','Inspection Date']
+df_edited = df[colunas_solicitadas]
 
-# Commit do job
+df_edited.rename(columns={'Name':'nome','Description':'descricao','Violation Type':'tipo_violacao','Inspection Date':'data_inspecao'})
+
+df_edited=df.replace('nan','vazio')
+
+spark_df = spark.createDataFrame(df_edited)
+
+spark_df.write.csv(s3_output_path_csv, mode='overwrite', header=True)
+
+spark_df.write.parquet(s3_output_path_name, mode='overwrite')
+
 job.commit()
